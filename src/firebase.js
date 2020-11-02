4698254048b1e3bd6379f53adf72aaf4661623fa
surendra477 <58383796+surendra477@users.noreply.github.com>
2020-11-02 18:36:35 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/database';
 import 'firebase/auth';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyD0mwJj6yKcOgsyJN3jIZCEVsThl6or3Yw",
-    authDomain: "surendraphonenum.firebaseapp.com",
-    databaseURL: "https://surendraphonenum.firebaseio.com",
-    projectId: "surendraphonenum",
-    storageBucket: "surendraphonenum.appspot.com",
-    messagingSenderId: "814662765416",
-    appId: "1:814662765416:web:2de5d2c139acc5b13124dd",
-    measurementId: "G-928Q4738DV"
+    apiKey:"" ,
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
